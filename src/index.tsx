9baf67322a0cc7e4c917eba3402cacec3e23a3c6
Matheus Matos <matheus@matheusmatos.com>
2025-01-29 15:17:23 -0300

    fix: native and fallback module return

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import Moderninha from './Moderninha';
 import type { IPlugPag } from './types/IPlugPag';
-import PlugPagFallback from './PlugPagFallback';
 import PlugPagNative from './PlugPagNative';
+import PlugPagFallback from './PlugPagFallback';
 
 export * from './types';
 
 const PlugPag: IPlugPag = Moderninha
-  ? new PlugPagFallback()
-  : new PlugPagNative();
+  ? new PlugPagNative()
+  : new PlugPagFallback();
 
 export default PlugPag;
